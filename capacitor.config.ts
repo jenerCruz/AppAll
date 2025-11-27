f328b42a72c2ce88ca5f7218f9db50da8a453b6a
jenerCruz <pamecoaxaca.telcdl@gmail.com>
2025-11-26 18:52:37 -0600

    rewyruxtursndo

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.app.all',
-  appName: 'AppAll',
-  webDir: 'www'
-};
-
-export default config;
--- www/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.appall.app',
-  appName: 'appall',
-  webDir: 'www'
-};
-
-export default config;
